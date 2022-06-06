d236bc2f56030c43da3fdef1075423fc7f6baf52
Norah Taylor <emt368@gmail.com>
2022-06-06 16:50:37 -0400

    commit tree 7c78f1cbe5903a8d09dd5a20660d4a5adb45f6c7

--- methods.rb
+++ methods.rb
@@ -1,1 +1,43 @@
 # Your code here!
+def my_method(param)
+    puts "running my_method"
+    param + 1
+end 
+
+#describe "MyRubyThing" do
+   # it "runs" do 
+#end 
+#end 
+
+def say_hi(name) 
+    puts "hi there, #{name}"
+end 
+say_hi("norah")
+
+def greet_programmer
+    puts "Hello, programmer!"
+end 
+
+def greet(name)
+    puts "Hello, #{name}!"
+end 
+greet("Jimmy")
+greet("Naureen")
+
+def greet_with_default(name = "programmer") 
+    puts "Hello, #{name}!"
+end 
+greet_with_default("Naureen")
+
+def add num1, num2
+    num1 + num2
+end
+
+add(2, 5)
+
+def halve number 
+    if number.class != Integer 
+        return nil 
+    end 
+    number / 2
+end 
